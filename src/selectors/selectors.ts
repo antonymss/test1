72430ef3d00e473608d0b7f8eab075e18b70da8e
antonymss <demeshkevichanton@gmail.com>
2021-05-19 18:19:46 +0300

    refact

--- src/selectors/selectors.ts
+++ src/selectors/selectors.ts
@@ -1,16 +1,3 @@
-// import { IGlobalState } from './state';
-//
-// interface IRootState extends IGlobalState { }
-//
-// export const selectCurrencies = (state: IRootState) => state.currency.currencies;
-// export const selectCurrentCurrency = (state: IRootState) => state.currency.currentCurrency;
-// export const selectIsBuying = (state: IRootState) => state.currency.isBuying;
-// export const selectAmountOfBYN = (state: IRootState) => state.currency.amountOfBYN;
-// export const selectAmountOFCurrency = (state: IRootState) => state.currency.amountOfCurrency;
-//
-//
-// export const selectCurrencyState = (state: IRootState) => state.currency;
-
 import {RootStoreType} from "../redux/store";
 
 export const dataApi = (state: RootStoreType) => state.dataReducer
